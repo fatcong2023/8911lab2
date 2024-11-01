6383076871df4f828dbe25ab579463821878602a
song <ottstormtrooper@gmail.com>
2024-10-31 23:54:35 -0400

    for mid term!

---
+++ src/main/java/xs8911mid/RestApiFunc.java
@@ -0,0 +1,24 @@
+package xs8911mid;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+import java.util.logging.Logger;
+
+public class RestApiFunc {
+    @FunctionName("RestApiFunction")
+    public HttpResponseMessage getHello(
+        @HttpTrigger(name = "req", 
+                     methods = {HttpMethod.GET}, 
+                     route = "get") HttpRequestMessage<Optional<String>> request,
+        final ExecutionContext context) {
+
+        Logger logger = context.getLogger();
+        logger.info("HTTP trigger function processed a request.");
+
+        // Return response with "hello word \n"
+        return request.createResponseBuilder(HttpStatus.OK)
+                      .body("hello word \n")
+                      .build();
+    }
+}
